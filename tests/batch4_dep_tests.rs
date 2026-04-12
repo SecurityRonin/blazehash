eb52a147c40fe6fc1f46cd373bed75dea286f7c6
Albert Hui <i@hack9u.online>
2026-04-13 01:43:00 +0800

    test(RED): add failing dep tests for ml-dsa, qrcode, image

---
+++ tests/batch4_dep_tests.rs
@@ -0,0 +1,19 @@
+#[test]
+fn ml_dsa_crate_available() {
+    // If this compiles, the dep is present
+    let _ = std::mem::size_of::<ml_dsa::KeyPair<ml_dsa::MlDsa65>>();
+}
+
+#[cfg(feature = "qr")]
+#[test]
+fn qrcode_crate_available() {
+    let qr = qrcode::QrCode::new(b"test").unwrap();
+    assert!(qr.width() > 0);
+}
+
+#[cfg(feature = "qr")]
+#[test]
+fn image_crate_available() {
+    let img = image::RgbImage::new(1, 1);
+    assert_eq!(img.width(), 1);
+}
